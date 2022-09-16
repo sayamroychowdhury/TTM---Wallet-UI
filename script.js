11d3e3fcfa176a4bee23f8963315b811a095dbcd
Sayam <mail2sayam127@gmail.com>
2022-09-16 13:07:45 +0530

    Additions

--- script.js
+++ script.js
@@ -15,3 +15,13 @@ let donutChart = new Chart(myChart, {
         }
     }
 });
+
+const order = document.querySelectorAll(".order");
+order.forEach((userItem) => {
+userItem.onclick = function() {myFunction()};
+function myFunction(){
+    const el = document.createElement('div');
+    el.textContent = 'View Details';
+    el.style.cssText = 'color:#000; background-color:#40EEB9; height:2rem;font-size: 0.75rem; display:flex; justify-content:center; align-items:center; margin: 0.5rem 0 0.75rem 0; border-radius: 0.19rem';
+    userItem.parentNode.appendChild(el);
+}});
